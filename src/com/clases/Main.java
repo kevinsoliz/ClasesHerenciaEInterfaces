75d4d3704dc7499650def612210b9a3530de7473
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-08 09:43:56 +0200

    Clase textBox: Para evitar esta excepción tienes que inicializar tus varibles a vacio o cero, etc.

--- src/com/clases/Main.java
+++ src/com/clases/Main.java
@@ -9,5 +9,8 @@ public class Main {
         var textBox = new TextBox(); //funciona! Usando var, puedes crear un código más limpio y que el compilador de java determine su tipo.
         //textBox.setText("Hello World");
         System.out.println(textBox.text.toUpperCase());
+        // lo que estamos intentando hacer es usar este método con un objeto que no referencia a un objeto real.
+        // por eso nos salta el nullPointerException
+        //Para evitar esta excepción tienes que inicializar tus varibles a vacio o cero, etc.
     }
 }
--- src/com/clases/TextBox.java
+++ src/com/clases/TextBox.java
@@ -1,7 +1,7 @@
 package com.clases;
 
 public class TextBox {
-    public String text; //por defecto si no inicializas esta variable estará establecida a null.
+    public String text = ""; //por defecto si no inicializas esta variable estará establecida a null.
                         //Los nulls son peligrosos y pueden crash our programs
 
     public void setText(String text) { // In situations where the name of the parameter is the same as the field we use this. keyword
