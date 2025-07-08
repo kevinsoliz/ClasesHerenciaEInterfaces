3702e2a5f30c32466692e95c510fd3ef20b07602
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-08 22:19:31 +0200

    Una manera rápida de saber si un número es par o impar

--- src/com/clases/Main.java
+++ src/com/clases/Main.java
@@ -7,10 +7,14 @@ public class Main {
         //Con var puedes declarar una variable y darle valores diferentes.
 
         var textBox = new TextBox(); //funciona! Usando var, puedes crear un código más limpio y que el compilador de java determine su tipo.
-        //textBox.setText("Hello World");
+        textBox.setText("Hello World");
         System.out.println(textBox.text.toUpperCase());
-        // lo que estamos intentando hacer es usar este método con un objeto que no referencia a un objeto real.
-        // por eso nos salta el nullPointerException
-        //Para evitar esta excepción tienes que inicializar tus varibles a vacio o cero, etc.
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Hello World");
+        System.out.println(textBox2.text);
+        int n = 1;
+        String[] parImpar = {"Par", "Impar"};
+        System.out.println(parImpar[n % 2]);
     }
 }
