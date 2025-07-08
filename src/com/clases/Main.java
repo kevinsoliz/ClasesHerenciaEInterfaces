997ddbb336904c7e69dc238055b6736d03a4372b
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-08 09:38:22 +0200

    Clase textBox: si no inicializas la variable String por defecto se establece a null y puede darte un null pointer Exception, es de las más famosas en Java

---
+++ src/com/clases/Main.java
@@ -0,0 +1,13 @@
+package com.clases;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //TextBox textBox = new TextBox(); // Hay otra manera de instanciar un clase creada, tienes que usar var
+        //Con var puedes declarar una variable y darle valores diferentes.
+
+        var textBox = new TextBox(); //funciona! Usando var, puedes crear un código más limpio y que el compilador de java determine su tipo.
+        //textBox.setText("Hello World");
+        System.out.println(textBox.text.toUpperCase());
+    }
+}
---
+++ src/com/clases/TextBox.java
@@ -0,0 +1,14 @@
+package com.clases;
+
+public class TextBox {
+    public String text; //por defecto si no inicializas esta variable estará establecida a null.
+                        //Los nulls son peligrosos y pueden crash our programs
+
+    public void setText(String text) { // In situations where the name of the parameter is the same as the field we use this. keyword
+        this.text = text;
+    }
+
+    public void clear() { // It's not necessary here
+        text = "";
+    }
+}
