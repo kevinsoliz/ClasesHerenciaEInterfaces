0ed34aff5ec61feb61b28ff5b25e69fc8460f64a
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-09 20:19:01 +0200

    Browser class: La OOP es una filosofía.

--- src/com/clases/Main.java
+++ src/com/clases/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
         //vamos a crear un nuevo objeto browser y para ello no vas a escribirlo todo, simplemente new Browser() y ya podras ver la bombilla.
         // y luego lo cambias a var.
-        var browser = new Browser();
+        var browser = new Browser(); //los dos metodos restantes serán private porque no los vamos a usar en el main.
+                                    //De esta manera la interfaz de este objeto es más simple.
+        //la oop es una manera de pensar, una filosofia. No solo un porrón de metodos con variables.
+
 
     }
 }
