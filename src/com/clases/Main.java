3efcca617946531de29a9f5a6357b1f34e202bc9
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-09 10:59:31 +0200

    Clase Employee: así es como se encapsula, y ahora la clase es reutilizable, recuerda que tener las funciones dentro de la misma clase Main se denomina programación procedimental

--- src/com/clases/Main.java
+++ src/com/clases/Main.java
@@ -3,18 +3,10 @@ package com.clases;
 
 public class Main {
     public static void main(String[] args) {
-        //TextBox textBox = new TextBox(); // Hay otra manera de instanciar un clase creada, tienes que usar var
-        //Con var puedes declarar una variable y darle valores diferentes.
-
-        var textBox = new TextBox(); //funciona! Usando var, puedes crear un código más limpio y que el compilador de java determine su tipo.
-        textBox.setText("Hello World");
-        System.out.println(textBox.text.toUpperCase());
-
-        var textBox2 = new TextBox();
-        textBox2.setText("Hello World");
-        System.out.println(textBox2.text);
-        int n = 1;
-        String[] parImpar = {"Par", "Impar"};
-        System.out.println(parImpar[n % 2]);
+        var employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 50;
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
     }
 }
