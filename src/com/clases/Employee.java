249cf98d5f34fcc9dfa616a75e1323e12d9fd693
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-09 11:11:21 +0200

    Clase Employee: Necesitamos los getters y setters para controlar las validaciones

--- src/com/clases/Employee.java
+++ src/com/clases/Employee.java
@@ -1,10 +1,16 @@
 package com.clases;
 
 public class Employee {
-    public int baseSalary;
+    private int baseSalary; // Para controlar las validaciones debemos cambiar el modificador de acceso a private.
     public int hourlyRate;
 
     public int calculateWage(int extraHours){ // Digamos que las horas extra es la única variable que no es fija
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary){ // con esto ya tenemos controladas las validaciones.
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Base salary must be greater than 0");
+        this.baseSalary = baseSalary;
     }
 }
--- src/com/clases/Main.java
+++ src/com/clases/Main.java
@@ -4,7 +4,7 @@ package com.clases;
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50_000;
+        employee.setBaseSalary(50_000);//Hay un problema, qué pasa si introducimos numeros negativos? Hay que validar estas entradas.
         employee.hourlyRate = 50;
         int wage = employee.calculateWage(10);
         System.out.println(wage);
