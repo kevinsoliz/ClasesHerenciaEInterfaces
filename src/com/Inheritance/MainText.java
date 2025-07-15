9fb50d80034fbe95e56d26059e5c81fe2a2ebe53
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-15 15:29:33 +0200

    Herencia: Upcasting

--- src/com/Inheritance/MainText.java
+++ src/com/Inheritance/MainText.java
@@ -4,9 +4,13 @@ import com.clases.TextBox;
 
 public class MainText {
     public static void main(String[] args) {
-        TextBox textBox = new TextBox(true);
-        textBox.setText("Hello World");
-        System.out.println(textBox); //no hace falta poner .toString!!!!!!!!!!!! joder! el compilador ya sabe lo que tiene que hacer!
+        var control = new UIControl(true);
+        var text = new TextBox(true);
+        text.setText("Hello World");
+        show(text); //el método funciona con text porque este es hijo de UIControl
+    }
+    public static void show (UIControl control) {
+        System.out.println(control);
 
     }
 }
