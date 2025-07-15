b95a0887cc58e57173a91ed3bb0c3b7c8c6ca23e
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-15 15:32:01 +0200

    Herencia: todos heredan de Object, is a relationship

--- src/com/Inheritance/MainText.java
+++ src/com/Inheritance/MainText.java
@@ -8,8 +8,9 @@ public class MainText {
         var text = new TextBox(true);
         text.setText("Hello World");
         show(text); //el método funciona con text porque este es hijo de UIControl
+                    //La herencia representa una "is a" relationship, text es un UIControl
     }
-    public static void show (UIControl control) {
+    public static void show (Object control) {
         System.out.println(control);
 
     }
