2e4bb6b3ec9dbbe409262b9e0b243d57bf770097
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-15 15:34:20 +0200

    Herencia: Downcasting

--- src/com/Inheritance/MainText.java
+++ src/com/Inheritance/MainText.java
@@ -6,11 +6,12 @@ public class MainText {
     public static void main(String[] args) {
         var control = new UIControl(true);
         var text = new TextBox(true);
-        text.setText("Hello World");
         show(text); //el método funciona con text porque este es hijo de UIControl
                     //La herencia representa una "is a" relationship, text es un UIControl
     }
     public static void show (Object control) {
+        var text = (TextBox) control; //Esto es un downcasting
+        text.setText("Hello World");
         System.out.println(control);
 
     }
