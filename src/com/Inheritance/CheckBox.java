721b69f6f16c0804dc43263e263ccd57f9257250
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-16 15:54:12 +0200

    CheckBox: final classes and methods

--- src/com/Inheritance/CheckBox.java
+++ src/com/Inheritance/CheckBox.java
@@ -1,9 +1,12 @@
 package com.Inheritance;
 
-public class CheckBox extends UIControl{
+public final class CheckBox extends UIControl{
 
     @Override
     public void render() {
         System.out.println("Rendering CheckBox");
     }
 }
+
+//public class MyCheckbox extends CheckBox(); //No funciona porque está declarada como final, pasa lo mismo con los métodos
+                                            //los que son final no se pueden sobreescribir
