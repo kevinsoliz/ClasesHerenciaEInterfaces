4fa4274b87c0d0aabeb1f06349a1ee58a1fa1dea
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-16 09:56:33 +0200

    Point: equals y hasdCode van siempre de la mano

--- src/com/Inheritance/MainPoint.java
+++ src/com/Inheritance/MainPoint.java
@@ -5,6 +5,7 @@ public class MainPoint {
         var point1 = new Point(1, 2);
         var point2 = new Point(1, 2);
 
-        System.out.println(point1.equals(point2));
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
     }
 }
--- src/com/Inheritance/Point.java
+++ src/com/Inheritance/Point.java
@@ -1,5 +1,7 @@
 package com.Inheritance;
 
+import java.util.Objects;
+
 public class Point {
     private int x;
     private int y;
@@ -10,8 +12,24 @@ public class Point {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        Point p = (Point) obj; // hay que hacer el downcasting si o si
-        return x == p.x && y == p.y;
+    public boolean equals(Object obj) { //El problema es que se podrá meter cualquier tipo de objeto y dará una ClasCastException
+                                        //Incluso se podría meter el mismo objeto.
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Point))
+            return false;
+        var other = (Point) obj;
+        return x == other.x && y == other.y;
+    }
+    //Genera un entero para indexar objetos Si a.equals(b) es true, entonces a.hashCode() == b.hashCode()
+    //Cuando usas clases como HashMap o HashSet, Java:
+    //Usa hashCode() para saber en qué "casilla" del hash ubicar el objeto.
+    //Luego usa equals() para verificar si realmente es el mismo objeto.
+
+    //hashCode y equals van siempre juntos.
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y); //ya lo tendrias.
     }
 }
