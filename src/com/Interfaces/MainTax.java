e67b527ef22ab5296db38e71ccfcffcdccbaab25
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-17 10:43:21 +0200

    MainTax: Method Injection

--- src/com/Interfaces/MainTax.java
+++ src/com/Interfaces/MainTax.java
@@ -2,17 +2,16 @@ package com.Interfaces;
 
 public class MainTax {
     public static void main(String[] args) {
-        //Constructor Injection: otra clase (MainTax) se encargará de pasar el objeto a la TaxReport
-        //para que en el futuro solo tengamos que modificar esta (MainTax).
-        //Aunque modifiquemos la TaxCalculator2025  y está tenga que ser recompilada, la TaxReport no tiene dependencia alguna con esta y
-        // no habrá problemas.
-        var calculator = new TaxCalculator2025(100_000);
-        var taxReport = new TaxReport(calculator);
-        taxReport.show();
 
-        //Setter injection: a lo largo de la vida de nuestro programa quizás necesites cambiar la dependencia:
-        taxReport.setCalculator(new TaxCalculator2026());
-        taxReport.show();
-        //Ahora la Constructor Injection es la más común.
+        var calculator = new TaxCalculator2025(100_000);
+        //Method Injection:
+        var taxReport = new TaxReport();
+        taxReport.show(calculator);
+        taxReport.show(new TaxCalculator2026());
+        //Pasas la injection a la clase que necesita esa dependencia.
+
+        //Todo genial pero aún hay dependencia peligrosa entre estas clases, estas dependen de la interfaz, y si la interfaz cambia
+        //todos se fastidian, las interfaces siempre deben ser pequeñas y no susceptibles de ser modificadas
+
     }
 }
--- src/com/Interfaces/TaxReport.java
+++ src/com/Interfaces/TaxReport.java
@@ -4,15 +4,15 @@ public class TaxReport {
 
     private TaxCalculator calculator;
 
-    public TaxReport(TaxCalculator calculator) { //Esto es lo que se conoce como Constructor Injection
-        this.calculator = calculator;
-    }
+//    public TaxReport(TaxCalculator calculator) { //Esto es lo que se conoce como Constructor Injection
+//        this.calculator = calculator;
+//    }
 
-    public void show(){
+    public void show(TaxCalculator calculator) { // Esto es Method Injection, porque solo este método necesita la calculator.
         var tax = calculator.calculateTax();
         System.out.println(tax);
     }
-    public void setCalculator(TaxCalculator calculator) { //Es es Setter Injection
-        this.calculator = calculator;
-    }
+//    public void setCalculator(TaxCalculator calculator) { //Es es Setter Injection
+//        this.calculator = calculator;
+//    }
 }
