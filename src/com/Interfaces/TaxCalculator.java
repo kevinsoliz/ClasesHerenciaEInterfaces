0bb26451e0698d04436c2107f03c38e2d2abf4b5
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-18 09:34:40 +0200

    TaxCalculator: bad feature in Java: final static fields

--- src/com/Interfaces/TaxCalculator.java
+++ src/com/Interfaces/TaxCalculator.java
@@ -1,6 +1,8 @@
 package com.Interfaces;
 
 public interface TaxCalculator {
+    float taxMax = 100; //Esto es una constante final y static, nunca debes poner constantes en tu interfaz, son
+                        //detalles de implementación y recuerda que una interfaz dice qué hace los métodos dicen cómo.
     double calculateTax(); // no hace falta poner modificadores de acceso, siempre va a ser público, una interfaz es un contrato público.
                             //Tampoco se puede instanciar.
 }
